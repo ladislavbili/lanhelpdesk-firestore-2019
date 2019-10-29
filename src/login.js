f9bc0f5e15b8ff9d213bd8d82009d18f99304930
senkovicova1 <36665069+senkovicova1@users.noreply.github.com>
2019-10-29 21:31:27 +0100

    SS-29.10.2019-ckeditor oprava + custom formats

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, Label,Input, Alert } from 'reactstrap';
+import { Button, FormGroup, Label, Input } from 'reactstrap';
 import firebase from 'firebase';
 import {rebase} from './index';
 import { connect } from "react-redux";
@@ -11,21 +11,29 @@ class Login extends Component {
 		this.state = {
 			email:'',
 			password:'',
-			working:false
+			working: false,
+			error: false,
 		};
 	}
 	render() {
 		return (
 			<div style={{height:'100vh',display: 'flex'}}>
 			<div className="card" style={{backgroundColor:'white', borderRadius:6, padding:'10px 20px', width:'350px',margin:'auto'}}>
+
 				<FormGroup>
 					<Label for="email">E-mail</Label>
-					<Input type="email" name="email" id="email" placeholder="Enter e-mail" value={this.state.email} onChange={(e)=>this.setState({email:e.target.value})} />
+					<Input type="email" name="email" id="email" placeholder="Enter e-mail" value={this.state.email} onChange={(e)=>this.setState({email:e.target.value, working: false})} />
 				</FormGroup>
 				<FormGroup>
 					<Label for="pass">Password</Label>
-					<Input type="password" name="pass" id="pass" placeholder="Enter password" value={this.state.password} onChange={(e)=>this.setState({password:e.target.value})} />
+					<Input type="password" name="pass" id="pass" placeholder="Enter password" value={this.state.password} onChange={(e)=>this.setState({password:e.target.value, working: false})} />
 				</FormGroup>
+				{
+					this.state.error &&
+					<div className="warning-general">
+							Wrong username or password!
+					</div>
+				}
 			<Button color="primary" disabled={this.state.working} onClick={()=>{
 					this.setState({error:false, working:true});
 					firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password).then((res)=>{
@@ -34,13 +42,14 @@ class Login extends Component {
 						rebase.get('users/'+res.user.uid, {
 			        context: this,
 			      }).then((user)=>this.props.setUserData(user));
-					}).catch(error=>{this.setState({error:true});console.log('error')});
+					}).catch(error=>{
+						this.setState({
+							error:true});
+						console.log(error)}
+					);
 				}}>Login</Button>
-			{
-				this.state.error && <Alert color="danger">
-        Wrong username or password!
-      </Alert>
-			}
+
+
 			</div>
 		</div>
 		);
